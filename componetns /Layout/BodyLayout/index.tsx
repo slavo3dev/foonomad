c63a61c56c80f89436adda148b73744e5720c7a2
slavo3dev <slavo@slavo3.com>
2021-07-05 11:38:33 -0400

    adding types

--- componetns /Layout/BodyLayout/index.tsx
+++ componetns /Layout/BodyLayout/index.tsx
@@ -1,7 +1,12 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import styles from "../../../styles/Home.module.css";
 
+interface Props {
+	children: any
+}
 
-export const BodyLayout = ( { children }: any) =>
+
+export const BodyLayout = ( { children }: Props) =>
 {
 	return (
 		<div className={styles.container}>
