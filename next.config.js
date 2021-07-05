c687d1c2bc2b3d91e01ff3b964fd3ab3e5ae0a35
slavo3dev <slavo@slavo3.com>
2021-07-04 21:53:18 -0400

    adding env

--- next.config.js
+++ next.config.js
@@ -1,9 +1,8 @@
-
-
+/* eslint-disable no-undef */
 module.exports = {
  
 	env: {
-		CONTENTFUL_SPACE_ID: process.env.CONTENTFUL_SPACE_ID,
-		CONTENTFUL_ACCESS_TOKEN: process.env.CONTENTFUL_ACCESS_TOKEN,
+		CONTENTFUL_SPACE_ID: process.env.NEXT_PUBLIC_SPACE_ID,
+		CONTENTFUL_ACCESS_TOKEN: process.env.NEXT_PUBLIC_ACCESS_TOKEN,
 	},
 };
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,16 +3,14 @@ import { Container } from "semantic-ui-react";
 import { ItemTemplate } from "../componetns ";
 import { createClient } from "contentful";
 
-
-
 const titleHeader = "NRG DAY";
 const textDescription = "Spending a healthy day in these times is according to many people impossible, but we are here to change that. It doesn’t matter how you lived your life before, the important thing is how you will start a new day.";
 const urlImage = "http://placekitten.com/200/100";
 const extraDitails = "Following a healthy, active lifestyle doesn’t have to be complicated! Even the smallest adjustment to your routine can lead to big changes."; 
 
 const client = createClient({
-	space: process.env.NEXT_SPACE_ID,
-	accessToken: process.env.NESXT_ACCESS_TOKEN
+	space: process.env.NEXT_PUBLIC_SPACE_ID,
+	accessToken: process.env.NEXT_PUBLIC_ACCESS_TOKEN
 } );
 
 export default function Home (props: any)
@@ -22,6 +20,11 @@ export default function Home (props: any)
 
 
 	console.log("Blog Articles: ", blogArticles);
+
+	console.log( "READ: ", {
+		space: process.env.NEXT_PUBLIC_SPACE_ID,
+		accessToken: process.env.NEXT_PUBLIC_ACCESS_TOKEN
+	});
 	return (
 		<Container>
 			<ItemTemplate titleHeader={titleHeader} description={textDescription} urlImage={urlImage} extraDitails={extraDitails }  />
