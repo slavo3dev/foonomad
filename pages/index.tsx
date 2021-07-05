2078a1d9e2a82e5b2534abe826e8892528ac85dd
slavo3dev <slavo@slavo3.com>
2021-07-05 10:16:28 -0400

    fixing

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,9 +6,11 @@ import { createClient } from "contentful";
 const titleHeader = "NRG DAY";
 const textDescription = "Spending a healthy day in these times is according to many people impossible, but we are here to change that. It doesn’t matter how you lived your life before, the important thing is how you will start a new day.";
 const urlImage = "http://placekitten.com/200/100";
-const extraDitails = "Following a healthy, active lifestyle doesn’t have to be complicated! Even the smallest adjustment to your routine can lead to big changes."; 
+const extraDitails = "Following a healthy, active lifestyle doesn’t have to be complicated! Even the smallest adjustment to your routine can lead to big changes.";
 
-const client = createClient({
+
+
+console.log( {
 	space: process.env.NEXT_PUBLIC_SPACE_ID,
 	accessToken: process.env.NEXT_PUBLIC_ACCESS_TOKEN
 } );
@@ -16,10 +18,9 @@ const client = createClient({
 export default function Home (props: any)
 {
 	const { blogArticles } = props;
+
 	
-
-
-	console.log("Blog Articles: ", blogArticles);
+	console.log("Blog Articles: ", props, blogArticles);
 
 	console.log( "READ: ", {
 		space: process.env.NEXT_PUBLIC_SPACE_ID,
@@ -33,19 +34,27 @@ export default function Home (props: any)
 }
 
 
-export async function getStaticProps() {
-	const data = client
+export async function getStaticProps ()
+{
+
+	const client = createClient({
+		space: process.env.NEXT_PUBLIC_SPACE_ID,
+		accessToken: process.env.NEXT_PUBLIC_ACCESS_TOKEN 
+	} );
+	
+	const data = await client
 		.getEntries({
 			content_type: "blog"
 		})
-		.then((entry: any) => console.log("ENTRY: ", entry))
+		.then((entry: any) =>  entry)
 		.catch((err: any)=> console.log("ERROR MESSAGE: ", err));
 
 	
 	return {
 		props: {
-			blogArticles: data.items
+			blogArticles: JSON.stringify(data)
 		}
 	};
 	
 }
+
