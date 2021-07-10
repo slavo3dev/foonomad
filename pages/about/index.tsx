6d2daa6b37c29d033894a2b47cc537e6dadc37bd
slavo3dev <slavo@slavo3.com>
2021-07-10 11:37:42 -0400

    contact page

---
+++ pages/about/index.tsx
@@ -0,0 +1,10 @@
+
+
+export default function about ()
+{
+	return (
+		<div>
+			<h1>About me Page</h1>
+		</div>
+	);
+}
---
+++ pages/contact/index.tsx
@@ -0,0 +1,8 @@
+export default function contact ()
+{
+	return (
+		<div>
+			<h1>Contact Page</h1>
+		</div>
+	);
+}
