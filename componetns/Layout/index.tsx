d107df2fd10cd2ff9ac17b41c2070b7e30971e4d
slavo3dev <slavo@slavo3.com>
2021-07-18 07:26:32 -0400

    fixing styles

--- componetns/Layout/index.tsx
+++ componetns/Layout/index.tsx
@@ -1,6 +1,6 @@
 import { Footer } from "./Footer";
 import { HeadComponent } from "./HeadComponent";
 import { BodyLayout } from "./BodyLayout";
-import { MainHeader } from "./MainHeader";
+import { MainHeader } from "./MainNavigation";
 
 export { Footer, HeadComponent, BodyLayout, MainHeader};
