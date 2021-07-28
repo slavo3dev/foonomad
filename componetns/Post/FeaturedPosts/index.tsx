35793d8bbcfe2d26606a4171b679cde415f38d63
slavo3dev <slavo@slavo3.com>
2021-07-28 16:12:40 -0400

    adding featured post

--- componetns/Post/FeaturedPosts/index.tsx
+++ componetns/Post/FeaturedPosts/index.tsx
@@ -5,7 +5,7 @@ export function FeaturedPosts ( { posts }: any) {
 	const { blogArticles } = posts;
 	const blogs = blogArticles.items;
 
-	console.log( "BLOGS: ", blogs );
+	
 	
 
 	const postUrl = ( slug: string ) =>
@@ -30,19 +30,26 @@ export function FeaturedPosts ( { posts }: any) {
     
 	return (
 		<Container>
-			<h1>Articles</h1>
+			<h1>Featured Posts</h1>
 			<Grid stackable>
 				<Grid.Row >
-					{ blogs.map( ( blog: any ) => (
-						<ArticaleCard
-							key={blog.fields.slug }
-							postUrl={ postUrl( blog.fields.slug ) }
-							titleHeader={ blog.fields.title }
-							description={ blog.fields.excerpt }
-							urlImage={ blog.fields.featureImage.fields }
-							extraInfo={ extra(blog.fields.timestamp) }
-							category={ blog.fields.category } />
-					)
+					{ blogs.map( ( blog: any ) =>
+					{
+						
+						if ( blog.fields.isFeatured === "true" )
+						{
+							return	(
+								<ArticaleCard
+									key={blog.fields.slug }
+									postUrl={ postUrl( blog.fields.slug ) }
+									titleHeader={ blog.fields.title }
+									description={ blog.fields.excerpt }
+									urlImage={ blog.fields.featureImage.fields }
+									extraInfo={ extra(blog.fields.timestamp) }
+									category={ blog.fields.category } />
+							);
+						} 
+					}
 					)
 					}
 				</Grid.Row>
