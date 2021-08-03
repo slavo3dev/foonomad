560ec3b85bbad6056e25c4ff5110bf7857041a33
slavo3dev <slavo@slavo3.com>
2021-08-03 10:21:50 -0400

    google analytics

--- componetns/Layout/HeadComponent/index.tsx
+++ componetns/Layout/HeadComponent/index.tsx
@@ -7,6 +7,22 @@ export const HeadComponent= () =>
 			<title>Software Web Development | ChatBot | SEO | Digital Marketing </title>
 			<link rel="icon" href="/nrgnomad.svg" />
 			<link href="https://fonts.googleapis.com/css2?family=Lobster&display=swap" rel="stylesheet" />
+			<script
+				async
+				src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+			/>
+			<script
+				dangerouslySetInnerHTML={{
+					__html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+              page_path: window.location.pathname,
+            });
+          `,
+				}}
+			/>
 		</Head>
 	);
 };
