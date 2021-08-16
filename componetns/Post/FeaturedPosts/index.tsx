a3573941576338608fe8146d329b181aac3709a7
slavo3dev <slavo@slavo3.com>
2021-08-16 16:11:16 -0400

    adding stuff

--- componetns/Post/FeaturedPosts/index.tsx
+++ componetns/Post/FeaturedPosts/index.tsx
@@ -1,7 +1,17 @@
 import { ArticaleCard } from "../../ArticaleCard";
-import { Container, Grid, Icon } from "semantic-ui-react";
+import { Container, Grid, Icon, Transition } from "semantic-ui-react";
+import { useState } from "react";
 
 export function FeaturedPosts ( { posts }: any) {
+	
+	const [ hide, setHide ] = useState( 500 );
+	const [ show, setShow ] = useState( 500 );
+	const [ visible, setVisible ] = useState( true );
+	
+
+	const toggleVisibility = () => setVisible(false);
+
+
 	const { blogArticles } = posts;
 	const blogs = blogArticles.items;
 
@@ -30,7 +40,7 @@ export function FeaturedPosts ( { posts }: any) {
     
 	return (
 		<>
-			<h1 style={ { padding: "15px", textAlign: "center" } }>Featured Posts</h1>
+			<h1 onClick={toggleVisibility} style={ { padding: "15px", textAlign: "center" } }>Featured Posts</h1>
 			<Container style={ { display:"flex", justifyContent: "center", width: "80%", margin: "auto"}}>
 				<Grid stackable columns={4} >
 					<Grid.Row >
@@ -41,13 +51,16 @@ export function FeaturedPosts ( { posts }: any) {
 								return	(
 									<div style={ { padding: "5px" } } key={ blog.fields.slug }>
 										<Grid.Column width={ 4 }>
-											<ArticaleCard
-												postUrl={ postUrl( blog.fields.slug ) }
-												titleHeader={ blog.fields.title }
-												description={ blog.fields.excerpt }
-												urlImage={ blog.fields.featureImage.fields }
-												extraInfo={ extra(blog.fields.timestamp) }
-												category={ blog.fields.category } />
+											<Transition 
+												duration={{ hide, show }} visible={visible}>
+												<ArticaleCard
+													postUrl={ postUrl( blog.fields.slug ) }
+													titleHeader={ blog.fields.title }
+													description={ blog.fields.excerpt }
+													urlImage={ blog.fields.featureImage.fields }
+													extraInfo={ extra(blog.fields.timestamp) }
+													category={ blog.fields.category } />
+											</Transition>
 										</Grid.Column>
 									</div>
 								);
