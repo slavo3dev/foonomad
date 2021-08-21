cfedbb203e2d46a1f4d92d659dd091c8d7c722e7
slavo3dev <slavo@slavo3.com>
2021-08-21 11:54:10 -0400

    adding Form Link and Adding forms

--- pages/contact/index.tsx
+++ pages/contact/index.tsx
@@ -1,8 +1,61 @@
+import { Formik } from "formik";
+import { Form, Button, Segment } from "semantic-ui-react";
+import { BasePage } from "../../componetns";
+ 
 export default function contact ()
 {
 	return (
-		<div>
-			<h1>Contact Page</h1>
-		</div>
+		<Segment stacked>
+			<BasePage title="Contact Page" />
+			<h1>We'd Love to Hear From You</h1>
+			<Formik
+				initialValues={{ name: "", email: "", message: ""}}
+				onSubmit={(values, actions) => {
+					setTimeout(() => {
+						alert(JSON.stringify(values, null, 2));
+						actions.setSubmitting(false);
+					}, 1000);
+				}}
+			>
+				{ props => (
+					<Form onSubmit={ props.handleSubmit }>
+						<Form.Input
+							label='Full Name'
+							fluid icon='user'
+							iconPosition='left'
+							type="text"
+							onChange={props.handleChange}
+							onBlur={props.handleBlur}
+							value={props.values.name}
+							name="name"
+							placeholder='Please, introduce yourself - Type Your Full Name '
+						/>
+						<Form.Input
+							label='Email'
+							fluid
+							icon='mail'
+							iconPosition='left'
+							type="email"
+							onChange={props.handleChange}
+							onBlur={props.handleBlur}
+							value={props.values.email}
+							name="email"
+							placeholder='Your Contact e-mail! example: slavo@slavo3.com'
+						/>
+						<Form.TextArea
+							label='Text Message'
+							type='text'
+							onChange={props.handleChange}
+							onBlur={props.handleBlur}
+							value={ props.values.message }
+							name="message"
+							placeholder="Please, ask how we can help or leave a message/comment"
+						/>
+						{ props.errors.name && <div id="feedback">{ props.errors.name }</div> }
+						<Button type="submit" color='blue' fluid size='large'>Submit</Button>
+					</Form>
+				)}
+			</Formik>
+		</Segment>
 	);
 }
