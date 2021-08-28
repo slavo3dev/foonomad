f11444079e3dee659d49e1045d92e118906df442
slavo3dev <slavo@slavo3.com>
2021-08-28 08:54:38 -0400

    fix ttypo

--- componetns/Hero /index.tsx
+++ componetns/Hero /index.tsx
@@ -15,7 +15,7 @@ export function Hero() {
 				/>
 			</div>
 			<h1>N-R-G Nomad</h1>
-			<p>You could leave life right now.<br />Let that determine what you do and say and think</p>
+			<p>You could live your life right now.<br />Let that determine what you do, say and think</p>
 			<Link href="/blog"><a><Button style={ { margin: "10px" } } size='big' inverted >Articles</Button></a></Link>
 		</section>
 	);
