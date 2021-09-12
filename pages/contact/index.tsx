61e11ea81f2689afec081d9cf0f4926509640712
slavo3dev <slavo@slavo3.com>
2021-09-12 11:59:39 -0400

    adding box shadow and simple styling

--- pages/contact/index.tsx
+++ pages/contact/index.tsx
@@ -1,7 +1,13 @@
-import {  ContactForm } from "../../componetns";
+import { ContactForm } from "../../componetns";
+import { Container } from "semantic-ui-react";
 
  
 export default function contact ()
 {
-	return <ContactForm />; 
+	return (
+		<Container style={{ margin: "5%", width: "45%", boxShadow: "12px 12px 2px 1px rgba(0, 102, 204, .1)" }}>
+			<ContactForm />
+		</Container>
+	);
+	
 }
