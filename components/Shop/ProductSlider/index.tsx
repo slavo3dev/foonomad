4b986dc0297d536481c2412ed8a546e7024b037b
slavo3dev <slavo@slavo3.com>
2021-10-02 18:34:15 -0400

    adding shop

--- components/Shop/ProductSlider/index.tsx
+++ components/Shop/ProductSlider/index.tsx
@@ -1,5 +1,6 @@
 import { ReactNode, FC } from "react";
 import styles from "./ProductSlider.module.css";
+import Ticker from "react-ticker";
 
 interface Props
 {
@@ -8,11 +9,19 @@ interface Props
 
 export const ProductSlider: FC<Props> = ({ children }) =>
 {
-     
-
 	return (
-		<div className={styles.pSliderContainer}>
-			{children}
+		<div className={ styles.pSliderContainer }>
+			<Ticker offset={200}>
+				{
+					() =>
+						<div className={ styles.pSliderRow }>
+				
+							{children}
+						</div>	
+				}
+			</Ticker>
 		</div>
+
+        
 	);
 };
