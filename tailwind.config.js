e5b5c7d2f0d834d6776742f2e3f057156e7c6743
slavo3dev <slavo@slavo3.com>
2021-11-09 21:03:59 -0500

    adding tailwind js

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+
+
+export const purge = ["./pages/**/*.{js,ts,jsx,tsx}","./components/**/*.{js,ts,jsx,tsx}"];
+export const darkMode = false;
+export const theme = {
+	extend: {},
+};
+export const variants = {
+	extend: {},
+};
+export const plugins = [];
