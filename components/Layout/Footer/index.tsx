a74dcb3d41d6a444c44abf3876185f6c7efa88a6
slavo3dev <slavo@slavo3.com>
2022-01-09 08:29:41 -0500

    adding new backlink

--- components/Layout/Footer/index.tsx
+++ components/Layout/Footer/index.tsx
@@ -8,15 +8,15 @@ export const Footer = () =>
 	return (
 		<Segment inverted vertical style={{ margin: "5em 0em 0em", padding: "5em 0em" }}>
 			<Container textAlign='center'>
-				<a href="https://www.slavo3.com" target="_blank" className={ classes.linkTag }><i>© { new Date().getFullYear()} Prototype.NEXT | Power by @slavo3  </i></a>
+				<a href="https://www.prototypenext.com" target="_blank" className={ classes.linkTag }><i>© { new Date().getFullYear()} Prototype.NEXT | Power by @slavo3  </i></a>
 				<Divider inverted section />
 				{/* <Image centered size='mini' src='/logo.png' alt="logo image" /> */}
 				<List horizontal inverted divided link size='small'>
 					<List.Item as='a' href='/about'> About Me</List.Item>
 					<List.Item as='a' href='/blog'>Blog</List.Item>
-					<List.Item as='a' href='/contact'>Contact Us</List.Item>
+					{/* <List.Item as='a' href='/contact'>Contact Us</List.Item>
 					<List.Item as='a' href='#'>Terms and Conditions</List.Item>
-					<List.Item as='a' href='#'>Privacy Policy</List.Item>
+					<List.Item as='a' href='#'>Privacy Policy</List.Item> */}
 				</List>
 			</Container>
 		</Segment>
